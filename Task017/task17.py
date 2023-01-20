8fe3e643703f5617b33208014193bf4a3c95e76c
Vyacheslav <vilisov.v.s@yandex.ru>
2023-01-20 16:31:21 +0700

    Разложение числа на простые множители

--- Task017/task17.py
+++ Task017/task17.py
@@ -1,7 +1,7 @@
 """
 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 """
-
+'''
 n = int(input('Введите натуральное число: '));
 delitel = 2;
 while n > 1:
@@ -10,4 +10,18 @@ while n > 1:
         print(delitel, end = " ");
     else:
         delitel = delitel + 1;
-    
+'''
+def PrimeFactors(n):
+    tempList = []
+    delitel = 2;
+    while n > 1:
+        if n%delitel==0:
+            n = n/delitel;
+            tempList.append(delitel)
+        else:
+            delitel = delitel + 1;
+    return tempList;
+
+resultList = [(n, PrimeFactors(n)) for n in range(4, 30)]
+
+print(resultList)
