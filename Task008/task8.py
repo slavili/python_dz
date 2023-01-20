8fb2c4496c8b21244ca1c46606c92b8d45492b57
Vyacheslav <vilisov.v.s@yandex.ru>
2023-01-20 15:39:37 +0700

    использовал list comprehension, map, list, lambda-функцию

--- Task008/task8.py
+++ Task008/task8.py
@@ -1,7 +1,7 @@
 """
 Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
 """
-
+'''
 def UserFunction(n):
     tempValue: float = 0.0;
 
@@ -15,6 +15,14 @@ userNumber = int(input('Введите целое число: '));
 
 for i in range(1, userNumber+1):
     userList.append(UserFunction(i));
+
+print(userList);
+print(sum(userList));
+'''
+
+userNumber = int(input('Введите целое число: '));
+
+userList = list(map(lambda n: round((1 + 1/n)**n, 2), [n for n in range(1, userNumber+1)]));
 
 print(userList);
 print(sum(userList));
