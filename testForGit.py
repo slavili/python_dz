f34cb85df55ea208b917ad23a144c87ad56f2809
Slavchik Slavili <slavchikv@rambler.ru>
2023-02-13 09:43:52 +0700

    Change main2

--- testForGit.py
+++ testForGit.py
@@ -4,5 +4,18 @@ function main1(){
 }
 //for user2
 function main2(){
-//
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
 }
