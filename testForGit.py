45760b04751fd4ff0a77965d311ff7f848acb666
Vyacheslav <vilisov.v.s@yandex.ru>
2023-02-13 09:43:33 +0700

    Change main1

--- testForGit.py
+++ testForGit.py
@@ -1,6 +1,19 @@
 //for user1
 function main1(){
-//
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
+//line of code
 }
 //for user2
 function main2(){
